f23e9435ebbce935871a0973234394c1ab07fae7
Deathlycraft <deathlycraft-github@deathlyenterprises.com>
2016-01-11 13:37:06 -0600

    Added ISerializer contract for serializing objects.

---
+++ Core/Serialization/ISerializer.cs
@@ -0,0 +1,8 @@
+﻿namespace DeathlyEnterprises.Core.Serialization
+{
+    public interface ISerializer<TSerializerType>
+    {
+        TSerializerType Serialize<TObject>(TObject objectToSerialize);
+        TObject Deserialize<TObject>(TSerializerType serializedObject);
+    }
+}
